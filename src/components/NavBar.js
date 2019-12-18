8a3be8424893fc3aa7a6f01e6ba8d5a6e525e12b
jastaffen <jack.staffen@gmail.com>
2019-12-18 16:42:01 -0500

    Done.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,10 +1,38 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 
+const link = {
+  margin: '10px'
+};
+
 const NavBar = () => {
   return (
-    <div>
-      {/*{code here}*/}
+    <div className="navbar">
+
+      <NavLink
+        to="/"
+        exact 
+        style={link}
+      >Home</NavLink>
+
+      <NavLink
+        to="/actors"
+        exact
+        style={link}
+      >Actors</NavLink>
+
+      <NavLink
+        to="/movies"
+        exact
+        style={link}
+      >Movies</NavLink>
+
+      <NavLink 
+        to="/directors"
+        exact
+        style={link}
+      >Directors</NavLink>
+
     </div>
   );
 };
